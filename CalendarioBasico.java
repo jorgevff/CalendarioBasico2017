21350073dff2bb892a3df4e609299857584462af
jorgevff <jvalladaresfontano@gmail.com>
2017-10-30 03:35:31 +0100

    Se crea la clase CalendarioBasico con el constructor

---
+++ CalendarioBasico.java
@@ -0,0 +1,25 @@
+
+/**
+ * se crea la cabezera de la  clase
+ */
+public class CalendarioBasico
+{
+    //se inicializa el dia
+    private int dia;
+    //se inicializa el mes   
+    private int mes;
+    //se inicializa el año
+    private int anio;
+
+    /**
+     * damos los valores de todos los parametros
+     */
+    public CalendarioBasico()
+    {
+
+        dia = 1;
+        mes = 1;
+        anio = 1;
+
+    }
+}
